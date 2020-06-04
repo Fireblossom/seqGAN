ac64d00186f9a92450484e7b6144ba97a87a47aa
Fireblossom <Fireblossom@users.noreply.github.com>
2020-06-04 10:00:20 +0200

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -52,7 +52,7 @@ def prepare_discriminator_data(pos_samples, neg_samples, gpu=False):
         - inp: (pos_size + neg_size) x seq_len
         - target: pos_size + neg_size (boolean 1/0)
     """
-    inp = torch.cat((pos_samples.to(dtype=torch.longint), neg_samples.to(dtype=torch.longint)), 0).type(torch.LongTensor)
+    inp = torch.cat((pos_samples.to(dtype=torch.long), neg_samples.to(dtype=torch.long)), 0).type(torch.LongTensor)
     target = torch.ones(pos_samples.size()[0] + neg_samples.size()[0])
     target[pos_samples.size()[0]:] = 0
 
