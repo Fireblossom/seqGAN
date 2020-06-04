28313b08b884874d6d2fe51969af549ef64923ec
Fireblossom <Fireblossom@users.noreply.github.com>
2020-06-04 10:06:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -127,16 +127,15 @@ def train_discriminator(discriminator, dis_opt, real_data_samples, generator, or
                         BATCH_SIZE)) / 10.) == 0:  # roughly every 10% of an epoch
                     print('.', end='')
                     sys.stdout.flush()
-            for i in range(0, len(labeled_data), BATCH_SIZE):
-                inp, target = labeled_data[i:i+BATCH_SIZE], label[i:i+BATCH_SIZE]
-                dis_opt.zero_grad()
-                _, feature = discriminator.batchClassify(inp)
-                cls = classfifier(feature.detach())
-                loss_clf = nn.CrossEntropyLoss()
-                loss_cls = loss_clf(cls, target)
-                loss_cls.backward()
-                dis_opt.step()
-                clf_acc += torch.sum((torch.argmax(cls, dim=1))==(label)).data.item()
+                    
+            dis_opt.zero_grad()
+            _, feature = discriminator.batchClassify(labeled_data)
+            cls = classfifier(feature.detach())
+            loss_clf = nn.CrossEntropyLoss()
+            loss_cls = loss_clf(cls, label)
+            loss_cls.backward()
+            dis_opt.step()
+            clf_acc += torch.sum((torch.argmax(cls, dim=1))==(label)).data.item()
 
             total_loss /= ceil(2 * POS_NEG_SAMPLES / float(BATCH_SIZE))
             total_acc /= float(2 * POS_NEG_SAMPLES)
